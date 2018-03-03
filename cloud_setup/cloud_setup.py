0724e8f52cc0326f5e1d7c58ebfe24e372eb8faf
raph-m <raphael.montaud@riminder.net>
2018-03-03 15:51:26 +0100

    evaluate my_basic_strategy

--- cloud_setup/cloud_setup.py
+++ cloud_setup/cloud_setup.py
@@ -5,20 +5,11 @@ sudo apt-get install python3-setuptools
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 alias python=python3
-sudo apt-get python3-setuptools
+# sudo apt-get python3-setuptools
 sudo easy_install3 pip
-sudo pip3 install pandas
-sudo pip3 install requests
-sudo pip3 install dotenv
-git clone https://github.com/raph-m/safe_driver_prediction
-cd safe_driver_prediction/proj2
-python gdrive.py 1EQ0zE_2WLQdNIepWUjroPyGmi-dvN5KK ../../data.zip
-cd ..
-cd ..
-sudo apt-get install unzip
-unzip data.zip
-cd safe_driver_prediction
-echo "ENV_NAME=vm" > .env
-cd proj2
-python feature_engineering.py
+sudo pip3 install numpy
+sudo pip3 install matplotlib
+git clone https://github.com/raph-m/blackjack
+cd blackjack
+
 """
