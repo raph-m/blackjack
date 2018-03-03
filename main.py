2ec72ac194fa67f3326fa3859e4c7df2c8f8ba48
raph-m <raphael.montaud@riminder.net>
2018-03-03 16:49:47 +0100

    parallel computing

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from strategy_generator.base_monte_carlo import MC
+# from strategy_generator.base_monte_carlo import MC
 import json
+
+# policy = MC(epochs=epochs, epsilon=1.0/alpha)
+# policy["name"] = "my_basic"
+# policy["epochs"] = epochs
+# print(policy)
+# with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "w") as fp:
+#     json.dump(policy, fp)
 epochs = 100000000
 alpha = 10
-policy = MC(epochs=epochs, epsilon=1.0/alpha)
-policy["name"] = "my_basic"
-policy["epochs"] = epochs
-print(policy)
-with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "w") as fp:
-    json.dump(policy, fp)
+with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "r") as fp:
+    policy = json.load(fp)
+
+from strategies.naive_strategy import parallel_expectancy
+n_tries = 10000000
+print(parallel_expectancy(policy, n_tries))
