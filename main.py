eb86e9892758478db9d2165e88ee6685db6232b2
alexis.rouge <alexis.rouge@polytechnique.edu>
2018-03-04 17:11:34 +0100

    clean code

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-# from strategy_generator.base_monte_carlo import MC
 import json
 
-# policy = MC(epochs=epochs, epsilon=1.0/alpha)
-# policy["name"] = "my_basic"
-# policy["epochs"] = epochs
-# print(policy)
-# with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "w") as fp:
-#     json.dump(policy, fp)
+"""
+# generate a policy with a Monte Carlo RL algorithm and store it in a json file
+from strategy_generator.base_monte_carlo import MC
+policy = MC(epochs=epochs, epsilon=1.0/alpha)
+policy["name"] = "my_basic"
+policy["epochs"] = epochs
+print(policy)
+with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "w") as fp:
+    json.dump(policy, fp)
+"""
+
+"""
+# generate a policy with a Q learning algorithm
+from strategy_generator.base_qlearning import QLearn
+epochs = 100000
+policy = QLearn(epochs=epochs, epsilon=0.1, alpha=0.1, gamma=0.99)
+policy["name"] = "my_basic"
+policy["epochs"] = epochs
+"""
+
+"""
+# Load a strategy designed with n epochs
 epochs = 100000000
 alpha = 10
 with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "r") as fp:
@@ -14,12 +29,22 @@ with open("strategy_tuning/"+str(epochs)+"_"+str(alpha)+".json", "r") as fp:
 
 print(policy["soft.19.4"])
 policy["name"] = "my_basic"
+"""
 
+"""
+# Evaluate the policy with parallel computing
 from strategies.naive_strategy import parallel_expectancy, expectancy
 import time
 start = time.time()
-n_tries = 10000000  # 00
+n_tries = 10000 # 00
 print(parallel_expectancy(policy, n_tries))
 end = time.time()
 print("parallel time: ")
 print(end-start)
+"""
+
+
+# implement dfo to find the better hyper parameters for MC and Q-learning
+from strategy_tuning.dfo_tuning import tune
+tune("qlearn")
+#tune("MC")
