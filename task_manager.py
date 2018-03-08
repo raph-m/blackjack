9bfafe237a35debe0faaeed4b3f7cd23bf6d3837
raph-m <raphael.montaud@riminder.net>
2018-03-08 02:34:37 +0100

    final commit

--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from strategies.strategies import best_naive_strategy, read_counter_results, plot_counter_parallel
 from strategies.compare_counters import compare_counters
@@ -6,15 +7,15 @@ from strategies.counters import PersonalizedCounter, hi_opt_1, hi_opt_2, omega_2
     my_counter0, my_counter1
 
 
-n = int(1e3)  #  1e5 TODO: you can reduce the values of n by a factor 10 if you want to obtain the results faster
+n = int(1e5)  # TODO: you can reduce the values of n by a factor 10 if you want to obtain the results faster
 # TODO: but they will be less precise...
 print("computing best naive strategy:")
 best_naive_strategy(n)
 
 from util.tools import visualizePolicy
-from strategies.naive_strategy import save_base_policy
-from strategies.naive_strategy import parallel_expectancy
-save_base_policy() #to be run once
+from strategies.strategies import save_base_policy
+from strategies.strategies import parallel_expectancy
+save_base_policy()  # to be run once
 #load the generated policy
 with open("strategy_generator/base_wiki_policy.json", "r") as fp:
     wiki_pol = json.load(fp)
@@ -24,7 +25,6 @@ wiki_pol["epochs"] = 1
 
 n_tries = int(1e6)
 print("expectancy for reference basic strategy:", parallel_expectancy(wiki_pol, n_tries))
-
 
 
 # implement dfo to find the best hyper parameters for MC and Q-learning
@@ -48,14 +48,14 @@ print("expectancy for Monte Carlo basic strategy:", parallel_expectancy(mc_pol, 
 
 from strategy_generator.base_qlearning import QLearn
 with open("strategy_tuning/qlearn_hyper_parameters.json", "r") as fp:
-    hyp_cl = json.load(fp)
+    hyp_ql = json.load(fp)
 ql_pol = QLearn(epochs=epochs, epsilon=hyp_ql["epsilon"], alpha=hyp_ql["alpha"], gamma=hyp_ql["gamma"])
 visualizePolicy(ql_pol)
 ql_pol["name"] = "my_basic"
 ql_pol["epochs"] = 1
 print("expectancy for Q-learning basic strategy:", parallel_expectancy(ql_pol, n_tries))
 
-n = int(1e3)  # 1e5
+n = int(1e5)
 number_of_decks = [2, 2, 3, 4, 4, 6]
 shuffle_every = [52, 80, 104, 52, 104, 80]
 ids = ["thorp"+str(number_of_decks[i])+"decks"+str(shuffle_every[i])+"shuffleevery" for i in range(len(number_of_decks))]
@@ -89,7 +89,7 @@ famous_counters_names.append("my_3_level_counter_with_b=6")
 number_of_decks = 4
 shuffle_every = 52
 
-n = int(1e4)  # 1e5
+n = int(1e5)
 for i in range(len(famous_counters)):
 
     # Counter parallel
@@ -124,7 +124,7 @@ end = time.time()
 print("done in: "+str((end-start) / (60 * 60)) + " hours")
 
 ratio = (end - start) / n
-n = int(1e4)  # 1e7
+n = int(1e7)
 print("running compare counters for n =" + str(n))
 start = time.time()
 print("this should take " + str(n * ratio / (60 * 60)) + " hours")
